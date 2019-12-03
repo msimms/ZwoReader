9374742bbcb40b59aec22f216347c73e267623e5
Mike Simms <msimms01@gmail.com>
2019-12-02 19:38:26 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+
+setup(
+    name='ZwoReader',
+    version=__version__,
+    description='.',
+    url='https://github.com/msimms/ZwoReader',
+    packages=[],
+    author='Mike Simms',
+    author_email='mike@mikesimms.net',
+    license='MIT',
+    install_requires=[],
+    python_requires='>=2.6'
+)
